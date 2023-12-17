a79bb2bb1d79694c830efd5efa9ef1901df60c8c
Terry Nguyen <terry@terrehbyte.com>
2023-12-16 19:47:24 -0800

    Fix bounce and expose function via map

--- Ease.gd
+++ Ease.gd
@@ -8,76 +8,76 @@ const c3 := c1 + 1;
 const c4 := (2 * PI) / 3;
 const c5 := (2 * PI) / 4.5;
 
-static func bounceOut(x):
+static func bounce_out(x):
 	var n1 = 7.5625
 	var d1 = 2.75
 	if x < 1 / d1:
 		return n1 * x * x
-	elif x < 2 / d1:
+	elif x < 2.0 / d1:
 		x -= 1.5 / d1
-		return n1 * x + 0.75
+		return n1 * x * x + 0.75
 	elif x < 2.5 / d1:
 		x -= 2.25 / d1
-		return n1 * x + 0.9375
+		return n1 * x * x + 0.9375
 	else:
 		x -= 2.625 / d1
-		return n1 * x + 0.984375
+		return n1 * x * x + 0.984375
 
 static func linear(x):
 	return x
 
-static func easeInQuad(x):
+static func in_quad(x):
 	return x * x
 
-static func easeOutQuad(x): 
+static func out_quad(x): 
 	return 1 - (1 - x) * (1 - x)
 
-static func easeInOutQuad(x): 
+static func in_out_quad(x): 
 	return 2 * x * x if x < 0.5 else 1 - pow(-2 * x + 2, 2) / 2
 
-static func easeInCubic(x): 
+static func in_cubic(x): 
 	return x * x * x
 
-static func easeOutCubic(x): 
+static func out_cubic(x): 
 	return 1 - pow(1 - x, 3)
 
-static func easeInOutCubic(x): 
+static func in_out_cubic(x): 
 	return 4 * x * x * x if x < 0.5 else 1 - pow(-2 * x + 2, 3) / 2
 
-static func easeInQuart(x): 
+static func in_quart(x): 
 	return x * x * x * x
 
-static func easeOutQuart(x): 
+static func out_quart(x): 
 	return 1 - pow(1 - x, 4)
 
-static func easeInOutQuart(x): 
+static func in_out_quart(x): 
 	return 8 * x * x * x * x if x < 0.5 else 1 - pow(-2 * x + 2, 4) / 2
 
-static func easeInQuint(x): 
+static func in_quint(x): 
 	return x * x * x * x * x
 
-static func easeOutQuint(x): 
+static func out_quint(x): 
 	return 1 - pow(1 - x, 5)
 
-static func easeInOutQuint(x): 
+static func in_out_quint(x): 
 	return 16 * x * x * x * x * x if x < 0.5 else  1 - pow(-2 * x + 2, 5) / 2
 
-static func easeInSine(x): 
+static func in_sine(x): 
 	return 1 - cos((x * PI) / 2)
 
-static func easeOutSine(x): 
+static func out_sine(x): 
 	return sin((x * PI) / 2)
 
-static func easeInOutSine(x): 
+static func in_out_sine(x): 
 	return -(cos(PI * x) - 1) / 2
 
-static func easeInExpo(x): 
+static func in_expo(x): 
 	return 0.0 if x == 0.0 else pow(2, 10 * x - 10)
 
-static func easeOutExpo(x): 
+static func out_expo(x): 
 	return 1.0 if x == 1.0 else 1 - pow(2, -10 * x)
 
-static func easeInOutExpo(x): 
+static func in_out_expo(x): 
 	if x == 0:
 		return 0
 	elif x == 1:
@@ -87,31 +87,31 @@ static func easeInOutExpo(x):
 	else:
 		return (2 - pow(2, -20 * x + 10)) / 2
 
-static func easeInCirc(x): 
+static func in_circ(x): 
 	return 1 - sqrt(1 - pow(x, 2))
 
-static func easeOutCirc(x): 
+static func out_circ(x): 
 	return sqrt(1 - pow(x - 1, 2))
 
-static func easeInOutCirc(x):
+static func in_out_circ(x):
 	if x < 0.5:
 		return (1 - sqrt(1 - pow(2 * x, 2))) / 2
 	else:
 		return (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2
 
-static func easeInBack(x): 
+static func in_back(x): 
 	return c3 * x * x * x - c1 * x * x
 
-static func easeOutBack(x): 
+static func out_back(x): 
 	return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2)
 
-static func easeInOutBack(x): 
+static func in_out_back(x): 
 	if x < 0.5:
 		return (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
 	else:
 		return (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2
 
-static func easeInElastic(x): 
+static func in_elastic(x): 
 	if x == 0:
 		return 0
 	elif x == 1:
@@ -119,7 +119,7 @@ static func easeInElastic(x):
 	else:
 		return -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4)
 
-static func easeOutElastic(x):
+static func out_elastic(x):
 	if x == 0:
 		return 0
 	elif x == 1:
@@ -127,7 +127,7 @@ static func easeOutElastic(x):
 	else:
 		return pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1
 
-static func easeInOutElastic(x):
+static func in_out_elastic(x):
 	if x == 0:
 		return 0
 	elif x == 1:
@@ -137,14 +137,14 @@ static func easeInOutElastic(x):
 	else:
 		return (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1
 
-static func easeInBounce(x): 
-	return 1 - bounceOut(1 - x)
+static func in_bounce(x): 
+	return 1 - bounce_out(1 - x)
 
-static func easeOutBounce(x):
-	return bounceOut(x)
+static func out_bounce(x):
+	return bounce_out(x)
 
-static func easeInOutBounce(x): 
+static func in_out_bounce(x): 
 	if x < 0.5:
-		return (1 - bounceOut(1 - 2 * x)) / 2
+		return (1 - out_bounce(1 - 2 * x)) / 2
 	else:
-		return (1 + bounceOut(2 * x - 1)) / 2
+		return (1 + out_bounce(2 * x - 1)) / 2
--- EasingDemo.gd
+++ EasingDemo.gd
@@ -11,48 +11,94 @@ var time_elapsed : float = 0.0
 
 enum EaseFunc {
 	BOUNCE_OUT,
+	
 	LINEAR,
-	EASE_IN_QUAD,
-	EASE_OUT_QUAD,
-	EASE_IN_OUT_QUAD,
-	EASE_IN_CUBIC,
-	EASE_OUT_CUBIC,
-	EASE_IN_OUT_CUBIC,
-	EASE_IN_QUART,
-	EASE_OUT_QUART,
-	EASE_IN_OUT_QUART,
-	EASE_IN_QUINT,
-	EASE_OUT_QUINT,
-	EASE_IN_OUT_QUINT,
-	EASE_IN_SINE,
-	EASE_OUT_SINE,
-	EASE_IN_OUT_SINE,
-	EASE_IN_EXPO,
-	EASE_OUT_EXPO,
-	EASE_IN_OUT_EXPO,
-	EASE_IN_CIRC,
-	EASE_OUT_CIRC,
-	EASE_IN_OUT_CIRC,
-	EASE_IN_BACK,
-	EASE_OUT_BACK,
-	EASE_IN_OUT_BACK,
-	EASE_IN_ELASTIC,
-	EASE_OUT_ELASTIC,
-	EASE_IN_OUT_ELASTIC,
-	EASE_IN_BOUNCE,
-	EASE_OUT_BOUNCE,
-	EASE_IN_OUT_BOUNCE,
+	
+	IN_QUAD,
+	OUT_QUAD,
+	IN_OUT_QUAD,
+	
+	IN_CUBIC,
+	OUT_CUBIC,
+	IN_OUT_CUBIC,
+	
+	IN_QUART,
+	OUT_QUART,
+	IN_OUT_QUART,
+	
+	IN_QUINT,
+	OUT_QUINT,
+	IN_OUT_QUINT,
+	
+	IN_SINE,
+	OUT_SINE,
+	IN_OUT_SINE,
+	
+	IN_EXPO,
+	OUT_EXPO,
+	IN_OUT_EXPO,
+	
+	IN_CIRC,
+	OUT_CIRC,
+	IN_OUT_CIRC,
+	
+	IN_BACK,
+	OUT_BACK,
+	IN_OUT_BACK,
+	
+	IN_ELASTIC,
+	OUT_ELASTIC,
+	IN_OUT_ELASTIC,
+
+	IN_BOUNCE,
+	OUT_BOUNCE,
+	IN_OUT_BOUNCE,
 }
 
 var ease_map = {
-	#EaseFunc.BOUNCE_OUT: Ease.bounce_out
+	EaseFunc.BOUNCE_OUT: Ease.bounce_out,
+	EaseFunc.LINEAR: Ease.linear,
+	EaseFunc.IN_QUAD: Ease.in_quad,
+	EaseFunc.OUT_QUAD: Ease.out_quad,
+	EaseFunc.IN_OUT_QUAD: Ease.in_out_quad,
+	EaseFunc.IN_CUBIC: Ease.in_cubic,
+	EaseFunc.OUT_CUBIC: Ease.out_cubic,
+	EaseFunc.IN_OUT_CUBIC: Ease.in_out_cubic,
+	EaseFunc.IN_QUART: Ease.in_quart,
+	EaseFunc.OUT_QUART: Ease.out_quart,
+	EaseFunc.IN_OUT_QUART: Ease.in_out_quart,
+	EaseFunc.IN_QUINT: Ease.in_quint,
+	EaseFunc.OUT_QUINT: Ease.out_quint,
+	EaseFunc.IN_OUT_QUINT: Ease.in_out_quint,
+	EaseFunc.IN_SINE: Ease.in_sine,
+	EaseFunc.OUT_SINE: Ease.out_sine,
+	EaseFunc.IN_OUT_SINE: Ease.in_out_sine,
+	EaseFunc.IN_EXPO: Ease.in_expo,
+	EaseFunc.OUT_EXPO: Ease.out_expo,
+	EaseFunc.IN_OUT_EXPO: Ease.in_out_expo,
+	EaseFunc.IN_CIRC: Ease.in_circ,
+	EaseFunc.OUT_CIRC: Ease.out_circ,
+	EaseFunc.IN_OUT_CIRC: Ease.in_out_circ,
+	EaseFunc.IN_BACK: Ease.in_back,
+	EaseFunc.OUT_BACK: Ease.out_back,
+	EaseFunc.IN_OUT_BACK: Ease.in_out_back,
+	EaseFunc.IN_ELASTIC: Ease.in_elastic,
+	EaseFunc.OUT_ELASTIC: Ease.out_elastic,
+	EaseFunc.IN_OUT_ELASTIC: Ease.in_out_elastic,
+	EaseFunc.IN_BOUNCE: Ease.in_bounce,
+	EaseFunc.OUT_BOUNCE: Ease.out_bounce,
+	EaseFunc.IN_OUT_BOUNCE: Ease.in_out_bounce
 }
+var cur_ease_func : Callable
+@export var ease_func : EaseFunc = EaseFunc.LINEAR
 
 func set_knob_position(pos : Vector2):
 	knob.set_anchor(SIDE_LEFT, pos.x, true)
 	knob.set_anchor(SIDE_TOP, pos.y, true)
 
 func _ready():
+	cur_ease_func = ease_map[ease_func]
+
 	line.clear_positions()
 
 	var slice = 1.0 / segments
@@ -62,7 +108,7 @@ func _ready():
 
 	for i in range(segments+1):
 		var x = i * slice
-		var y = Ease.linear(x)
+		var	y = cur_ease_func.call(x)
 		y = remap(y, 0.0, 1.0, 1.0, 0.0)
 
 		var new_pos = LinePosition.new()
@@ -78,6 +124,6 @@ func _process(_delta):
 	time_elapsed += _delta
 	var anim_time = wrapf(time_elapsed, 0.0, duration)
 	var ease_time = inverse_lerp(0.0, duration, anim_time)
-	var ease_y = remap(ease_time, 0.0, 1.0, 1.0, 0.0)
+	var ease_y = cur_ease_func.call(ease_time)
+	ease_y = remap(ease_y, 0.0, 1.0, 1.0, 0.0)
 	set_knob_position(Vector2(ease_time, ease_y))
-
